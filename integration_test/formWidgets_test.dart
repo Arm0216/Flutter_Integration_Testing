6d50d89d1f6e00ade17e8128579746e506e5ef2e
Sargis.Taranyan <sargis.taranyan@payever.org>
2021-04-30 22:53:30 +0400

    fix4

--- integration_test/formWidgets_test.dart
+++ integration_test/formWidgets_test.dart
@@ -1,11 +1,11 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import '../test/pages/formSamples_page.dart';
-import '../test/pages/formWidgets_page.dart';
+// import '../test/pages/formWidgets_page.dart';
 
 void main() {
   FormSamples formSamples = new FormSamples();
-  FormWidgets formWidgets = new FormWidgets();
+  // FormWidgets formWidgets = new FormWidgets();
 
   group('Form widgets', () {
     final binding = IntegrationTestWidgetsFlutterBinding.ensureInitialized()
@@ -16,5 +16,30 @@ void main() {
       await formSamples.clickOnFormWidgetsButton(tester);
       expect(find.text("Form widgets"), findsOneWidget);
     });
+
+    // testWidgets('Drop Slider Estimated Value', (tester) async {
+    //   await formSamples.clickOnFormWidgetsButton(tester);
+    //   await formWidgets.dropSliderEstimatedValue(tester);
+    //   expect(formWidgets.moneySlider, findsOneWidget);
+    // });
+    //
+    // testWidgets('Verify Enable Feature', (tester) async {
+    //   await formSamples.clickOnFormWidgetsButton(tester);
+    //   await formWidgets.clickOnSwitchFromEnableFeature(tester);
+    //   expect(formWidgets.activeSwitchEnableFeature, findsOneWidget);
+    // });
+    //
+    // testWidgets('Verify Edit Date button functionality', (tester) async {
+    //   await formSamples.clickOnFormWidgetsButton(tester);
+    //   await formWidgets.clickOnEditDateButton(tester);
+    //   expect(formWidgets.dayNine, findsOneWidget);
+    // });
+    //
+    // testWidgets('Make sure the button is selected', (tester) async {
+    //   await formSamples.clickOnFormWidgetsButton(tester);
+    //   await formWidgets.clickOnEditDateButton(tester);
+    //   await formWidgets.selectDayAndClickOnOkButton(tester);
+    //   expect(formWidgets.dayMonthYear, findsOneWidget);
+    // });
   });
 }
